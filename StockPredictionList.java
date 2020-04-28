42f0163a194fd423255eab2634bbe8c2891facd3
mailsivaamutha <64205632+mailsivaamutha@users.noreply.github.com>
2020-04-28 11:15:08 +0530

    Update and rename Test.java to StockPredictionList.java

--- StockPredictionList.java
+++ StockPredictionList.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class Test {
+public class StockPredictionList {
 	public static void main(String args[]){
 		List<Integer> stockData   = new ArrayList<Integer>(Arrays.asList(89214, 26671, 75144, 32445, 13656, 66289, 21951, 10265, 59857, 59133, 63227, 86121, 37411, 54628, 25859, 43510, 63756, 54763, 30852, 53243, 76238, 96885, 33074, 17745, 81814, 43436, 79172, 92819, 30001, 68442, 
 				54021, 35566, 95113, 29164, 84362, 25120, 11804, 6313, 51736, 71661, 81797, 14962, 57781, 35560, 85941, 99991, 
@@ -16,12 +16,8 @@ public class Test {
 				26, 18, 87, 56, 55, 97, 59, 62, 73, 58, 85, 8, 60, 87, 89, 89, 22));
 		
 		List<Integer> predictList = new ArrayList<Integer>();
-/*		List<Integer> headStockData = new ArrayList<Integer>();
-		List<Integer> tailStockData = new ArrayList<Integer>();*/
 		int samllestStockPrice = Collections.min(stockData);
 		int queryDayStockPrice = 0;
-/*		int headMin = 0;
-		int tailMin = 0;*/
 		
 		System.out.println("samllestStockPrice:"+samllestStockPrice);
 		for(int i=0; i<queries.size(); i++){
@@ -33,17 +29,11 @@ public class Test {
 			int nearHeadDay = 0;
 			int nearTailDay = 0;
 			queryDayStockPrice = stockData.get(day-1);
-			System.out.println(day+","+"queryDayStockPrice:"+queryDayStockPrice);
-/*			headStockData = stockData.subList(0, queries.get(i)-1);
-			System.out.println("headStockData:"+headStockData);
-			tailStockData = stockData.subList(queries.get(i), stockData.size());
-			System.out.println("tailStockData:"+tailStockData);*/
 			if(queryDayStockPrice == samllestStockPrice)
 				predictList.add(-1);
 			else{
 				System.out.println("else"+i);
 				for(int j=day-2; j>=0; j--){
-					System.out.println("headMin"+j);
 					if(stockData.get(j) < queryDayStockPrice){
 						headMin = stockData.get(j);
 						headMinDay = j+1;
@@ -51,14 +41,12 @@ public class Test {
 					}
 				}
 				for(int j=day; j<stockData.size(); j++){
-					System.out.println("tailMin"+j);
 					if(stockData.get(j) < queryDayStockPrice){
 						tailMin = stockData.get(j);
 						tailMinDay = j+1;
 						break;
 					}
 				}
-				System.out.println("headMin:"+headMin+"|tailMin:"+tailMin+"|headMinDay:"+headMinDay+"|tailMinDay:"+tailMinDay);
 				if(headMinDay == 0)
 					predictList.add(tailMinDay);
 				else if(tailMinDay == 0)
@@ -66,17 +54,14 @@ public class Test {
 				else {
 					nearHeadDay = day - headMinDay;
 					nearTailDay = tailMinDay - day;
-					System.out.println("nearHeadDay:"+nearHeadDay+"|nearTailDay:"+nearTailDay);
 					if(nearHeadDay == nearTailDay)
 					{
 						if(headMinDay == Math.min(headMinDay, tailMinDay))
 						{
-							System.out.println("headMinDay:"+headMinDay);
 							predictList.add(headMinDay);
 						}
 						else
 						{
-							System.out.println("tailMinDay:"+tailMinDay);
 							predictList.add(tailMinDay);
 						}
 					}
@@ -89,58 +74,6 @@ public class Test {
 				}
 			}
 
-			//predictList.add(queryDayStockPrice);
 		}
-		System.out.println("predictList:"+predictList);
-		
-		
-	    // Write your code here
-/*        int scheduledMeetings = 0;
-        Set<Integer> scheduledDays = new HashSet<Integer>();
-        //List<Integer> scheduledDays = new ArrayList<Integer>();
-        int investorSize = arrival.size();
-        List<Integer> modArrival =  new ArrayList<Integer>();
-        List<Integer> modDeparture =  new ArrayList<Integer>();
-
-        for(int i=0; i<investorSize; i++){
-            if(arrival.get(i) == departure.get(i)){
-                ++scheduledMeetings;
-                scheduledDays.add(arrival.get(i));
-            }
-            else{
-                modArrival.add(arrival.get(i));
-                modDeparture.add(departure.get(i));
-            }
-        }
-        int notScheduledSize = modArrival.size();
-
-        for(int i=0; i<notScheduledSize; i++){
-        	if(scheduledMeetings < investorSize){
-        		int arrive = modArrival.get(i);
-        		int depart = modDeparture.get(i);
-                if(!scheduledDays.contains(arrive)){
-                    ++scheduledMeetings;
-                    scheduledDays.add(arrive);
-                }
-                else if(!scheduledDays.contains(depart)){
-                    ++scheduledMeetings;
-                    scheduledDays.add(depart);
-                }
-        	}
-        	else
-        		break;
-        }
-
-
-        for(int i=0; i<arrival.size(); i++){
-        	for(int j=departure.get(i); j>=arrival.get(i); j--){
-        		if(!scheduledDays.contains(j)){
-        			scheduledMeetings++;
-        			scheduledDays.add(j);
-        			break;
-        		}
-        	}
-        }
-		System.out.println(scheduledMeetings);*/
 	}
 }
